10dd2a774dddf633c2427d4e0a609acbab15acf4
Arnav Agrawal <32594107+ArnavHP18@users.noreply.github.com>
2020-05-21 18:08:27 +0530

    Added CreateProject popup

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import Info from './Card.js'
-import './Dashboard.css'
-import carditems from './CardData.js'
-import Pie from './Pie.js'
-import Column from './Column.js'
-// import plus from './Images/plus.png'
-import {Table ,Container , Row , Col , Card , Button} from 'react-bootstrap'
+import React from 'react';
+import Info from './Card.js';
+import './Dashboard.css';
+import carditems from './CardData.js';
+import Pie from './Pie.js';
+import Column from './Column.js';
+// import plus from './Images/plus.png';
+import {Table ,Container , Row , Col , Card , Button} from 'react-bootstrap';
+import CreateProject from '../CreateProject';
+
 class Dashboard extends React.Component{
     constructor()
     {
@@ -19,36 +21,39 @@ class Dashboard extends React.Component{
         const CardComponent = this.state.items.map(item => <Info id = {item.key} item = {item}/>)
         return(
             <div>
-              
-             <Card className = "d"> 
-             <Card.Header as = "h4">Projects<Button style = {{marginLeft : "72%" , fontSize : "17px"}}>Create a new Project</Button></Card.Header>
-            <Table responsive className = "ProjectCard">
-                {CardComponent}
-            </Table>
-            </Card>
-            
-            <Container style = {{marginTop : "4%" , marginBottom : "2%"}}>
-          <Row>
-              <Col sm = {6} md = {6}>
-                <Card>
-                  <Card.Header as="h5">Project Distribution</Card.Header>
-                  <Card.Body>
-                    <Pie />
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col sm = {6} md = {6}>
-              <Card>
-                  <Card.Header as="h5">Project Models</Card.Header>
-                  <Card.Body>
-                    <Column />
-                  </Card.Body>
-                </Card>
-              </Col>
-          </Row>
-          </Container>
 
-      </div>
+              <Card className = "d">
+                <Card.Header as = "h4">Projects
+                  {/* <Button style={ { marginLeft:"72%", fontSize: "18px" }}>Create Project</Button> */}
+                  <CreateProject />
+                </Card.Header>
+                <Table responsive className = "ProjectCard">
+                  {CardComponent}
+                </Table>
+              </Card>
+
+              <Container style = {{marginTop : "4%" , marginBottom : "2%"}}>
+                <Row>
+                  <Col sm = {6} md = {6}>
+                    <Card>
+                      <Card.Header as="h5">Project Distribution</Card.Header>
+                      <Card.Body>
+                        <Pie />
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                  <Col sm = {6} md = {6}>
+                    <Card>
+                      <Card.Header as="h5">Project Models</Card.Header>
+                      <Card.Body>
+                        <Column />
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </Row>
+              </Container>
+
+            </div>
         )
     }
 }
